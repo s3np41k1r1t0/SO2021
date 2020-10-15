7532a14226cf06e447a336a576e30b365756b975
s3np41k1r1t0 <brunomendes@tecnico.ulisboa.pt>
2020-10-15 20:57:57 +0100

    improved performance by changing thread pool struct

--- main.c
+++ main.c
@@ -96,57 +96,69 @@ void applyCommand(){
         exit(EXIT_FAILURE);
     }
 
-    const char* command = removeCommand();
+    while (numberCommands > 0){
 
+        const char* command = removeCommand();
+
+        if(pthread_mutex_unlock(&mutex_comandos) != 0){
+            fprintf(stderr,"Error unlocking mutex\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if (command == NULL){
+            return;
+        }
+
+        char token, type;
+        char name[MAX_INPUT_SIZE];
+        int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+        if (numTokens < 2) {
+            fprintf(stderr, "Error: invalid command in Queue\n");
+            exit(EXIT_FAILURE);
+        }
+
+        int searchResult;
+        switch (token) {
+            case 'c':
+                switch (type) {
+                    case 'f':
+                        printf("Create file: %s\n", name);
+                        create(name, T_FILE);
+                        break;
+                    case 'd':
+                        printf("Create directory: %s\n", name);
+                        create(name, T_DIRECTORY);
+                        break;
+                    default:
+                        fprintf(stderr, "Error: invalid node type\n");
+                        exit(EXIT_FAILURE);
+                }
+                break;
+            case 'l': 
+                searchResult = lookup(name);
+                if (searchResult >= 0)
+                    printf("Search: %s found\n", name);
+                else
+                    printf("Search: %s not found\n", name);
+                break;
+            case 'd':
+                printf("Delete: %s\n", name);
+                delete(name);
+                break;
+            default: { /* error */
+                fprintf(stderr, "Error: command to apply\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    
+        if(pthread_mutex_lock(&mutex_comandos) != 0){
+            fprintf(stderr,"Error locking mutex\n");
+            exit(EXIT_FAILURE);
+        }
+    }
     if(pthread_mutex_unlock(&mutex_comandos) != 0){
         fprintf(stderr,"Error unlocking mutex\n");
         exit(EXIT_FAILURE);
-    }
-
-    if (command == NULL){
-        return;
-    }
-
-    char token, type;
-    char name[MAX_INPUT_SIZE];
-    int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
-    if (numTokens < 2) {
-        fprintf(stderr, "Error: invalid command in Queue\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int searchResult;
-    switch (token) {
-        case 'c':
-            switch (type) {
-                case 'f':
-                    printf("Create file: %s\n", name);
-                    create(name, T_FILE);
-                    break;
-                case 'd':
-                    printf("Create directory: %s\n", name);
-                    create(name, T_DIRECTORY);
-                    break;
-                default:
-                    fprintf(stderr, "Error: invalid node type\n");
-                    exit(EXIT_FAILURE);
-            }
-            break;
-        case 'l': 
-            searchResult = lookup(name);
-            if (searchResult >= 0)
-                printf("Search: %s found\n", name);
-            else
-                printf("Search: %s not found\n", name);
-            break;
-        case 'd':
-            printf("Delete: %s\n", name);
-            delete(name);
-            break;
-        default: { /* error */
-            fprintf(stderr, "Error: command to apply\n");
-            exit(EXIT_FAILURE);
-        }
     }
 }
 
@@ -154,24 +166,21 @@ void applyCommands(){
     pthread_t tid[numberThreads];
     memset(&tid,0,sizeof(tid));
 
-    while (numberCommands > 0){
-        for(int i = 0; i < numberThreads && numberCommands > 0; i++){    
-            if(pthread_create(&(tid[i]),NULL,(void *) &applyCommand,NULL) != 0){
-                fprintf(stderr,"Error creating thread\n");
-                exit(EXIT_FAILURE);
-            }
+    for(int i = 0; i < numberThreads && numberCommands > 0; i++){    
+        if(pthread_create(&(tid[i]),NULL,(void *) &applyCommand,NULL) != 0){
+            fprintf(stderr,"Error creating thread\n");
+            exit(EXIT_FAILURE);
         }
+    }
 
-        for(int i = 0; i < numberThreads; i++){
-            if(tid[i] != 0){
-                if(pthread_join(tid[i],NULL) != 0){
-                    fprintf(stderr,"Error joining thread\n");
-                    exit(EXIT_FAILURE);
-                }; 
-            }
-            tid[i] = 0;
+    for(int i = 0; i < numberThreads; i++){
+        if(tid[i] != 0){
+            if(pthread_join(tid[i],NULL) != 0){
+                fprintf(stderr,"Error joining thread\n");
+                exit(EXIT_FAILURE);
+            }; 
         }
-           
+        tid[i] = 0;
     }
 }
 
