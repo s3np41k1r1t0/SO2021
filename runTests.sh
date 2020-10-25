d63575890e10d139b54e07e1e3424d0925b2e32c
Bruno Mendes <30960262+s3np41k1r1t0@users.noreply.github.com>
2020-10-25 16:57:29 +0000

    updated runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -3,14 +3,37 @@
 inputdir=$1
 outputdir=$2
 maxthreads=$3
-mkdir $outputdir
+usage="Usage: ./runTests.sh <inputdir> <outputdir> numthreads"
+
+if test ! -f "tecnicofs"; then
+    make
+fi
+
+if test $# -ne 3; then
+    echo $usage
+    exit 1
+fi
+
+if test ! -d $inputdir; then
+   echo "Make sure your input directory exists"
+   exit 1
+fi
+
+if test $maxthreads -lt 1; then
+    echo "Invalid number of threads, must be greater than 0"
+    exit 1
+fi
+
+if test ! -d $outputdir; then
+    mkdir $outputdir
+fi
 
 for input in $inputdir/*
 do
+    inputfile=$(echo $input | cut -d / -f2)
     for i in $(seq 1 $maxthreads)
     do
         echo Inputfile=$input NumThreads=$i
-        ./tecnicofs $input $input-$i.txt $i | tail -1
-        mv $input-$i.txt $outputdir
+        ./tecnicofs $input $outputdir/$inputfile-$i.txt $i | tail -1
     done
 done
