76b910f4b51a052ed45087fcd6e3e17b5c177187
s3np41k1r1t0 <s3np41k1r1t0@lynx.Home>
2020-11-15 18:05:50 +0000

    updated runTests

--- runTests.sh
+++ runTests.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-#TODO remove this
-
 inputdir=$1
 outputdir=$2
 maxthreads=$3
-mkdir $outputdir
+usage="Usage: ./runTests.sh <inputdir> <outputdir> numthreads"
+
+if test ! -f "tecnicofs"; then
+    echo "Please run make to create executable"
+    exit 1
+fi
+
+if test $# -ne 3; then
+    echo $usage
+    exit 1
+fi
+
+if test ! -d $inputdir; then
+   echo "Make sure the input directory exists"
+   exit 1
+fi
+
+if test $maxthreads -lt 1; then
+    echo "Invalid number of threads, must be greater than 0"
+    exit 1
+fi
+
+if test ! -d $outputdir; then
+    echo "Make sure the output directory exists"
+    exit 1
+fi
 
 for input in $inputdir/*
 do
+    inputfile=$(echo $input | rev | cut -d / -f1 | rev)
     for i in $(seq 1 $maxthreads)
     do
-        echo Inputfile=$input NumThreads=$i
-        #valgrind --tool=helgrind
-        ./tecnicofs $input $input-$i.txt $i | tail -1
-        #echo -ne "\n\n\n\n\n\n\n\n"
-        mv $input-$i.txt $outputdir
+        echo Inputfile=$inputfile NumThreads=$i
+        ./tecnicofs $input $outputdir/$(echo $inputfile | cut -d . -f1)-$i.txt $i | tail -1
     done
 done
